1152fc74cb73fb0b4b2abf18ef1e5166a446ce0a
Minsoo Cho <textign@gmail.com>
2024-12-26 22:09:25 +0900

    main css 변경

--- src/components/common/layout/page/Page.tsx
+++ src/components/common/layout/page/Page.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from "react";
 
 function Page({ children }: PropsWithChildren) {
-  return <div className="max-w-[1440px] px-[48px] mx-auto min-h-screen">{children}</div>;
+  return <div className="max-w-[1440px] mx-auto min-h-screen">{children}</div>;
 }
 
 export default Page;
