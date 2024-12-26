91b9c47e5c70bd4fd97fcf13c8cc8b6e0dd746f2
Minsoo Cho <textign@gmail.com>
2024-12-26 22:19:09 +0900

    router로 변경 및 work list id 수정

--- src/components/work/detail/DetailWrapper.tsx
+++ src/components/work/detail/DetailWrapper.tsx
@@ -1,13 +1,16 @@
+"use client";
 import Image from "next/image";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { PropsWithChildren, memo } from "react";
 function DetailWrapper({ children }: PropsWithChildren) {
+  const router = useRouter();
+
   return (
     <div className="py-[48px]">
       <div className="flex justify-end">
-        <Link href="/work">
+        <button onClick={() => router.back()}>
           <Image src="/btn-close.svg" alt="닫기" width={64} height={64} />
-        </Link>
+        </button>
       </div>
       {children}
     </div>
--- src/constants/work.ts
+++ src/constants/work.ts
@@ -1,13 +1,13 @@
 export const WORK_LIST = [
   {
-    id: 1,
+    id: 5,
     category: "Frontend",
     title: "펫데이지",
     url: "https://pet-daisy.vercel.app/",
     tags: ["#1인가구", "#라이프스타일", "#반려동물"],
   },
   {
-    id: 2,
+    id: 4,
     category: "Frontend",
     title: "혼자살때 시즌 2",
     url: "https://livingalone2.vercel.app/",
@@ -21,14 +21,14 @@ export const WORK_LIST = [
     tags: ["#1인가구", "#라이프스타일", "#커뮤니티"],
   },
   {
-    id: 4,
+    id: 2,
     category: "Publishing",
     title: "장인성의 말랑한 오피스",
     url: "https://erinmzo.github.io/insungchang/",
     tags: ["#마케팅", "#포트폴리오"],
   },
   {
-    id: 5,
+    id: 1,
     category: "Publishing",
     title: "나응식의 묘집함",
     url: "https://erinmzo.github.io/naeungsic/",
