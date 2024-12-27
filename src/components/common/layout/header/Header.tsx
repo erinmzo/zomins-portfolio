f9fde59e9cfb4e06708d28364234028053b61241
Minsoo Cho <textign@gmail.com>
2024-12-27 20:29:54 +0900

    레이아웃 체크중

--- src/components/common/layout/header/Header.tsx
+++ src/components/common/layout/header/Header.tsx
@@ -3,9 +3,11 @@ import GlobalNav from "./GlobalNav";
 
 function Header() {
   return (
-    <div className="w-full max-w-[1440px] px-[48px] mx-auto fixed pt-[24px] pb-[20px] flex justify-between">
-      <Logo />
-      <GlobalNav />
+    <div className="max-w-[1440px] px-[48px] mx-auto fixed">
+      <div className="w-full flex justify-between pt-[24px] pb-[20px]">
+        <Logo />
+        <GlobalNav />
+      </div>
     </div>
   );
 }
