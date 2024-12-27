217aaf83817b820778717467fcd936927ff44ec8
Minsoo Cho <textign@gmail.com>
2024-12-27 21:04:16 +0900

    폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={`${pretendard} antialiased`}>{children}</body>
+      <body className={`${pretendard.className} antialiased`}>{children}</body>
     </html>
   );
 }
