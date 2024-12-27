f47392927f89788adede8c4263a44287b2ab918a
Minsoo Cho <textign@gmail.com>
2024-12-27 20:30:57 +0900

    레이아웃 수정

--- src/components/common/layout/header/Header.tsx
+++ src/components/common/layout/header/Header.tsx
@@ -3,7 +3,7 @@ import GlobalNav from "./GlobalNav";
 
 function Header() {
   return (
-    <div className="max-w-[1440px] px-[48px] mx-auto fixed">
+    <div className="w-full max-w-[1440px] px-[48px] mx-auto fixed">
       <div className="w-full flex justify-between pt-[24px] pb-[20px]">
         <Logo />
         <GlobalNav />
