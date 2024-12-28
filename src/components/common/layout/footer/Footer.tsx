bac8733804b740d0d7152e8db4fba3f25d555fd8
Minsoo Cho <textign@gmail.com>
2024-12-29 02:07:09 +0900

    오타 수정

--- src/components/common/layout/footer/Footer.tsx
+++ src/components/common/layout/footer/Footer.tsx
@@ -6,7 +6,7 @@ function Footer() {
     <footer className="bg-black pt-[16px] pb-[20px] md:py-[48px] ">
       <div className="w-full max-[390px] md:max-w-[1440px] px-4 md:px-12 mx-auto">
         <div className="text-blue text-[40px] md:text-[104px] leading-[48px] md:leading-none text-center md:text-left">
-          Thank you for Whatching.
+          Thank you for Watching.
         </div>
         <div className="md:hidden text-base text-blue mt-[24px] text-center">
           <a href={MAIL_TO}>zomins@naver.com</a>
