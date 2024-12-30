169aa12d1b2c619b197c345b0ff5a79abc3fdb5e
Minsoo Cho <textign@gmail.com>
2024-12-30 15:21:52 +0900

    leadign 추가

--- src/components/work/detail/Description.tsx
+++ src/components/work/detail/Description.tsx
@@ -2,7 +2,9 @@ interface DescriptionProps {
   description: string;
 }
 function Description({ description }: DescriptionProps) {
-  return <div className="whitespace-pre-line text-[16px] md:text-[28px] leading-[26px]">{description}</div>;
+  return (
+    <div className="whitespace-pre-line text-[16px] md:text-[28px] leading-[26px] md:leading-[42px]">{description}</div>
+  );
 }
 
 export default Description;
