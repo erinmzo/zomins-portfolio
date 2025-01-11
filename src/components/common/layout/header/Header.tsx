217219d021a30efafb1bbfc5b775e044e02defa9
Minsoo Cho <textign@gmail.com>
2025-01-11 14:47:57 +0900

    pulll

--- src/components/common/layout/header/Header.tsx
+++ src/components/common/layout/header/Header.tsx
@@ -23,7 +23,7 @@ function Header() {
   if (pathname === "/menu") {
     return (
       <div className="w-full fixed left-0 right-0 top-0 z-10">
-        <div className="w-full min-w-[340px] max-w-[390px] md:max-w-[1440px] px-4 md:px-12 md:mx-auto flex justify-end pt-[24px] pb-[20px] items-center">
+        <div className="w-full min-w-[340px] max-w-[390px] md:max-w-[1440px] px-4 md:px-12 mx-auto flex justify-end pt-[24px] pb-[20px] items-center">
           <button onClick={() => router.back()}>
             <svg width="24" height="22" viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg">
               <line
