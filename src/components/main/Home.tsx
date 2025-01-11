94e1f03b085f1264dd5e18afee09241607a21a86
Minsoo Cho <textign@gmail.com>
2025-01-11 14:14:55 +0900

    수정

--- src/components/main/Home.tsx
+++ src/components/main/Home.tsx
@@ -8,14 +8,14 @@ function Home() {
       <div className="hidden md:block pt-[140px] pb-[40px]">
         <h2 className="text-[96px] flex flex-col justify-center">
           <div>
-            <span className="inline-block py-4 px-10 rounded-[28px] bg-blue text-white">FRONTEND DEVELOPER</span>
+            <span className="inline-block py-3 px-10 rounded-[28px] bg-blue text-white">FRONTEND DEVELOPER</span>
           </div>
-          <div className="mt-[96px] flex flex-col items-start">
-            <span className="inline-block py-4 px-10 rounded-[28px] bg-black text-white">MINSOO CHO</span>
-            <span className="inline-block mt-8 py-4 px-10 rounded-[28px] bg-black text-white">PORTPOLIO</span>
+          <div className="mt-[88px] flex flex-col items-start">
+            <span className="inline-block py-3 px-10 rounded-[28px] bg-black text-white">MINSOO CHO</span>
+            <span className="inline-block mt-8 py-3 px-10 rounded-[28px] bg-black text-white">PORTPOLIO</span>
           </div>
         </h2>
-        <div className="flex flex-col justify-center items-center mt-[176px] md:mt-[158px] animate-bounce">
+        <div className="flex flex-col justify-center items-center mt-[176px] md:mt-[144px] animate-bounce">
           <span className="font-medium text-[24px]">SCROLL</span>
           <span>
             <Image src="/arrow.svg" alt="화살표" width={34} height={20} />
