8d3a254b7d81b3cafe7fa321529b609b6236ae15
Minsoo Cho <textign@gmail.com>
2025-01-11 19:28:27 +0900

    사이트맵 수정

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,26 +1,30 @@
 import { MetadataRoute } from "next";
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  // 캐시 방지를 위해 동적 날짜 생성
+  const currentDate = new Date().toISOString();
+
+  // 작업 배열 생성
   const works = [1, 2, 3, 4, 5].map((id) => ({
     url: `https://zomins-portfolio.vercel.app/work/${id}`,
-    lastModified: new Date("2025-01-11T10:16:16+00:00"),
+    lastmod: currentDate,
     priority: id > 2 ? 0.8 : 0.64,
   }));
 
   return [
     {
       url: "https://zomins-portfolio.vercel.app",
-      lastModified: new Date("2025-01-11T10:16:16+00:00"),
+      lastmod: currentDate,
       priority: 1.0,
     },
     {
       url: "https://zomins-portfolio.vercel.app/work",
-      lastModified: new Date("2025-01-11T10:16:16+00:00"),
+      lastmod: currentDate,
       priority: 0.8,
     },
     {
       url: "https://zomins-portfolio.vercel.app/menu",
-      lastModified: new Date("2025-01-11T10:16:16+00:00"),
+      lastmod: currentDate,
       priority: 0.8,
     },
     ...works,
