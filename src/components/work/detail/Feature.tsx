deeb5b3e0d9dfbc81dedc9fb2ff557144f2185c1
Minsoo Cho <textign@gmail.com>
2025-01-11 15:13:58 +0900

    detail

--- src/components/work/detail/Feature.tsx
+++ src/components/work/detail/Feature.tsx
@@ -8,10 +8,10 @@ function Feature({ features }: FeatureProps) {
   return (
     <div className="mt-[48px] md:mt-[160px]">
       <h3 className="font-bold text-[28px] md:text-[56px]">FEATURE</h3>
-      <ol className="flex flex-col gap-4 md:gap-5 mt-[16px] md:mt-[44px]">
+      <ol className="flex flex-col gap-4 md:gap-5 mt-[16px] md:mt-[32px]">
         {features.map((feature, index) => (
-          <li key={index} className="py-5 px-4 md:p-[40px] bg-[#fffbe8] rounded-[12px] md:rounded-[20px]">
-            <h4 className="text-[24px] md:text-[44px] font-bold">{feature.title}</h4>
+          <li key={index} className="py-5 px-4 md:px-[40px] md:py-[36px] bg-[#fffbe8] rounded-[12px] md:rounded-[20px]">
+            <h4 className="text-[24px] md:text-[36px] font-bold">{feature.title}</h4>
             <p className="text-[16px] md:text-[28px] whitespace-pre-line leading-[26px] md:leading-[1.3] mt-[36px] md:mt-[64px]">
               {feature.description}
             </p>
--- src/components/work/detail/TroubleShooting.tsx
+++ src/components/work/detail/TroubleShooting.tsx
@@ -9,20 +9,20 @@ function TroubleShooting({ troubleShooting }: TroubleShootingProps) {
   return (
     <div className="mt-[48px] md:mt-[160px]">
       <h3 className="font-bold text-[28px] md:text-[56px] leading-none">TROUBLE SHOOTING</h3>
-      <ul className="flex flex-col mt-[24px] md:mt-[44px] border-t border-black">
+      <ul className="flex flex-col mt-[24px] md:mt-[32px] border-t border-black">
         {troubleShooting.map((trouble, index) => (
           <li key={index} className="flex flex-col gap-8 md:gap-10 border-b border-black py-4 md:py-10">
-            <h4 className="max-w-max px-3 md:px-4 py-3 bg-black text-white font-bold text-[14px] md:text-[20px] rounded-lg">
+            <h4 className="max-w-max px-3 md:px-4 py-2 bg-black text-white font-bold text-[14px] md:text-[20px] rounded-lg">
               {trouble.title}
             </h4>
             <div className="text-base md:text-[28px] flex flex-col gap-8 md:gap-12">
               <div>
                 <span className="font-bold">문제점</span>
-                <p className="leading-[26px] md:leading-normal">{trouble.problem}</p>
+                <p className="mt-2 leading-[26px] md:leading-normal">{trouble.problem}</p>
               </div>
               <div>
                 <span className="font-bold">해결</span>
-                <p className="leading-[26px] md:leading-normal">{trouble.solution}</p>
+                <p className="mt-2 leading-[26px] md:leading-normal">{trouble.solution}</p>
               </div>
             </div>
           </li>
