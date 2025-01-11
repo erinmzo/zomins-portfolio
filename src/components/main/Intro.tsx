ded9e7b7807d20f0d81a0df5db506d47bebc0a65
Minsoo Cho <textign@gmail.com>
2025-01-11 13:42:26 +0900

    소개 문구 변경

--- src/components/main/Intro.tsx
+++ src/components/main/Intro.tsx
@@ -13,8 +13,8 @@ function Intro() {
           </div>
         </h3>
         <p className="text-white text-left md:text-center text-[20px] md:text-[32px] leading-relaxed font-medium md:w-[85%] md:break-normal">
-          디자인과 웹 퍼블리싱 경험을 통해 사용자 경험과 인터페이스의 중요성을 깊이 이해하게 되었고, 이를 기반으로
-          직관적이고 사용하기 편리한 웹 애플리케이션을 개발하는 데 집중하고 있습니다. 현재는 Next와 Tailwind를 활용한
+          디자인과 웹 퍼블리싱 경험을 바탕으로 원활한 협업을 이끌어내는 프론트엔드 개발자입니다. 동료들과의 적극적인
+          소통과 지식 공유를 통해 함께 성장하는 개발 문화를 만들어가고자 합니다. 현재는 Next.js와 Tailwind CSS를 활용한
           프론트엔드 개발에 주력하며, 기술적 도전을 통해 성장하는 것을 즐기고 있습니다.
         </p>
       </div>
