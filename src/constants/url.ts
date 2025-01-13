e39e17af768d6bebfa2fda0c4470d0a04e3463e8
Minsoo Cho <textign@gmail.com>
2025-01-13 11:49:58 +0900

    favicon

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,4 +1,4 @@
-export const RESUME_URL = "https://swanky-find-76e.notion.site/164f0495803c80b18719e872eecf852e?pvs=4";
+export const RESUME_URL = "https://drive.google.com/file/d/1FJAKPEpeEZd1orOz0_M2ZodGzcAe31n9/view?usp=sharing";
 export const BLOG_URL = "https://zomins.tistory.com/";
 export const GITHUB_URL = "https://github.com/erinmzo/";
 export const MAIL_TO = "mailto:zomins.naver.com";
