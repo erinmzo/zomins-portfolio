e88f5f1ee086b2dac9615216983acbcc059c35e7
Minsoo Cho <textign@gmail.com>
2025-01-13 11:50:19 +0900

    push

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,4 +1,4 @@
 export const RESUME_URL = "https://drive.google.com/file/d/1FJAKPEpeEZd1orOz0_M2ZodGzcAe31n9/view?usp=sharing";
 export const BLOG_URL = "https://zomins.tistory.com/";
 export const GITHUB_URL = "https://github.com/erinmzo/";
-export const MAIL_TO = "mailto:zomins.naver.com";
+export const MAIL_TO = "mailto:zomins@naver.com";
