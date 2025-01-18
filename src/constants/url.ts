2c98a2321b086d1e674463b3c6171f138ee3c2d3
Minsoo Cho <textign@gmail.com>
2025-01-18 17:06:02 +0900

    이력서 링크 수정

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,4 +1,4 @@
-export const RESUME_URL = "https://drive.google.com/file/d/1Pl2VuZq9KjZ3Nb0sb46CFUy0Boq5UXf9/view?usp=sharing";
+export const RESUME_URL = "https://drive.google.com/file/d/1Nfffcz3AY9FCwSUavBPkrqAdEqpF5hvw/view?usp=sharing";
 export const BLOG_URL = "https://zomins.tistory.com/";
 export const GITHUB_URL = "https://github.com/erinmzo/";
 export const MAIL_TO = "mailto:zomins@naver.com";
